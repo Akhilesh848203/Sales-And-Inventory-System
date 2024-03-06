ae73aab211fe73a5d6bdb81ca579c004024dc4bd
Administrator <Administrator@AD>
2024-03-06 14:57:37 +0530

    Added new feature

---
+++ src/test/java/com/Customer/SampleTest.java
@@ -0,0 +1,23 @@
+package com.Customer;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sample() {
+		System.out.println("----sample---");
+	}
+	
+	@Test
+	public void demo() {
+		System.out.println("----Demo---");
+	}
+	
+	@Test
+	public void test() {
+		System.out.println("----Test---");
+	}
+	
+	
+}
---
+++ src/test/java/com/Customer/Sample_2_Test.java
@@ -0,0 +1,21 @@
+package com.Customer;
+
+import org.testng.annotations.Test;
+
+public class Sample_2_Test {
+
+	@Test
+	public void sample2_demo1() {
+		System.out.println("----sample---");
+	}
+	
+	@Test
+	public void sample2_demo2() {
+		System.out.println("----sample---");
+	}
+	
+	@Test
+	public void sample2_demo3() {
+		System.out.println("----sample---");
+	}
+}
