6d1586ba176fedf7001b5006da91b1e10a3ddef0
Administrator <Administrator@AD>
2024-03-06 15:23:31 +0530

    Added

--- src/test/java/com/Customer/SampleTest.java
+++ src/test/java/com/Customer/SampleTest.java
@@ -6,17 +6,17 @@ public class SampleTest {
 
 	@Test
 	public void sample() {
-		System.out.println("----sample---");
+		System.out.println("----sample1---");
 	}
 	
 	@Test
 	public void demo() {
-		System.out.println("----Demo---");
+		System.out.println("----Demo2---");
 	}
 	
 	@Test
 	public void test() {
-		System.out.println("----Test---");
+		System.out.println("----Test3---");
 	}
 	
 	
